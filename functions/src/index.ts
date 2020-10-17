8b9c650e8b43d1763ba715ba3738ba0ba9deb804
DedicatedDev <topdev323@gmail.com>
2020-10-18 01:55:06 +0300

    version migration to 3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -39,49 +39,55 @@ export const sendToMyMembersNotification = functions.firestore
     // const flagClosed = connect!.flagClosed
 
     const roomId = context.params.roomId;
-    let possibleUsersRef = db.collection("users");
-    let masterRef = db.collection("Rooms").doc(roomId);
-    let videoRef = db.collection("videos");
-    let addInfoA: MemberInfo = { id: "", token: "", userName: "" };
-    let addInfoB: MemberInfo = { id: "", token: "", userName: "" };
-    let addInfoC: MemberInfo = { id: "", token: "", userName: "" };
-    let addInfoD: MemberInfo = { id: "", token: "", userName: "" };
+    const possibleUsersRef = db.collection("users");
+    const masterRef = db.collection("Rooms").doc(roomId);
+    const videoRef = db.collection("videos");
+    const addInfoA: MemberInfo = { id: "", token: "", userName: "" };
+    const addInfoB: MemberInfo = { id: "", token: "", userName: "" };
+    const addInfoC: MemberInfo = { id: "", token: "", userName: "" };
+    const addInfoD: MemberInfo = { id: "", token: "", userName: "" };
 
-    var cnt: number = 4;
+    let cnt: number = 4;
     let activityType: string = "";
     let activityDuration: string = "";
+    let videoType: string = "";
     let youtubeLink: string = "";
-    var tempCnt: number = 0;
+    let tempCnt: number = 0;
     let udid: string = "";
+    let masterName: String = "";
+    let topics:Array<String> = [];
     let videoPlayerId: number = 0;
     let selectedId: number = 0;
     let videoLink: string = "";
     let masterFcmToken: string = "";
-    var currentMembers: MyMembersInfo = {
+    let currentMembers: MyMembersInfo = {
       a: addInfoA,
       b: addInfoB,
       c: addInfoC,
-      d: addInfoD
+      d: addInfoD,
     };
-    await masterRef.get().then(snapshot => {
+    await masterRef.get().then((snapshot) => {
       activityType = snapshot.get("activityType");
+      videoType = activityType;
       activityDuration = snapshot.get("activityDuration");
       udid = snapshot.get("master");
+      masterName = snapshot.get("masterName");
       currentMembers = snapshot.get("members");
+      topics = snapshot.get("topics")
 
       if (typeof currentMembers === typeof undefined) {
         currentMembers = { a: addInfoA, b: addInfoB, c: addInfoC, d: addInfoD };
       }
-      if (currentMembers.a.id != "") {
+      if (currentMembers.a.id !== "") {
         tempCnt += 1;
       }
-      if (currentMembers.b.id != "") {
+      if (currentMembers.b.id !== "") {
         tempCnt += 1;
       }
-      if (currentMembers.c.id != "") {
+      if (currentMembers.c.id !== "") {
         tempCnt += 1;
       }
-      if (currentMembers.d.id != "") {
+      if (currentMembers.d.id !== "") {
         tempCnt += 1;
       }
     });
@@ -89,25 +95,50 @@ export const sendToMyMembersNotification = functions.firestore
     await possibleUsersRef
       .doc(udid)
       .get()
-      .then(snapshot => {
+      .then((snapshot) => {
         videoPlayerId = Number(snapshot.get("videoId"));
         videoLink = String(snapshot.get("videoLink"));
         masterFcmToken = String(snapshot.get("fcmToken"));
       });
 
-    await videoRef
-      .where("category", "==", activityType)
+    if(topics.length != 0){
+      await videoRef
+      .where("category", "==", videoType)
       .where("time", "==", activityDuration)
+      .where("topic", 'in',topics)
       .get()
-      .then(snapshot => {
-        let numberOfLinks = snapshot.docs.length;
+      .then((snapshot) => {
+        const numberOfLinks = snapshot.docs.length;
         if (numberOfLinks === 0) {
           youtubeLink = "";
         } else {
           selectedId = getRandomInt(numberOfLinks, videoPlayerId);
           youtubeLink = snapshot.docs[selectedId].get("url");
+          if(videoLink == youtubeLink){
+            delete snapshot.docs[selectedId]
+            selectedId = getRandomInt(numberOfLinks -1, videoPlayerId)
+            youtubeLink = snapshot.docs[selectedId].get("url");
+          }
         }
       });
+    }else{
+      await videoRef
+      .where("category", "==", videoType)
+      .where("time", "==", activityDuration)
+      .get()
+      .then((snapshot) => {
+        const numberOfLinks = snapshot.docs.length;
+        if (numberOfLinks === 0) {
+          youtubeLink = "";
+        } else {
+          selectedId = getRandomInt(numberOfLinks, videoPlayerId);
+          youtubeLink = snapshot.docs[selectedId].get("url");
+          delete snapshot.docs[selectedId];
+          selectedId = getRandomInt(numberOfLinks -1, videoPlayerId);
+        }
+      });
+    }
+
 
     await db
       .collection("users")
@@ -116,11 +147,11 @@ export const sendToMyMembersNotification = functions.firestore
 
     if (videoLink === youtubeLink) {
       await videoRef
-        .where("category", "==", activityType)
+        .where("category", "==", videoType)
         .where("time", "==", activityDuration)
         .get()
-        .then(snapshot => {
-          let numberOfLinks = snapshot.docs.length;
+        .then((snapshot) => {
+          const numberOfLinks = snapshot.docs.length;
           if (numberOfLinks === 0) {
             youtubeLink = "";
           } else {
@@ -138,143 +169,140 @@ export const sendToMyMembersNotification = functions.firestore
       .where("joined", "==", false)
       .where("goal", "==", activityType)
       .get()
-      .then(snapshot => {
+      .then((snapshot) => {
         if (snapshot.empty) {
           console.log("No matching documents.");
           return;
         }
 
-        let n = snapshot.docs.length;
-
+        const n = snapshot.docs.length;
+        console.log("total members");
+        console.log(n);
         if (n < 4) {
           cnt = n;
         }
 
-        for (let i = 0; i < n; i++) {
-          let memberFullInfo = snapshot.docs[i];
-          if (memberFullInfo.get("fcmToken") === masterFcmToken) continue;
-          if (
-            memberFullInfo.get("udid") === currentMembers.a.id ||
-            memberFullInfo.get("fcmToken") === currentMembers.a.token
-          )
-            continue;
-          if (
-            memberFullInfo.get("udid") === currentMembers.b.id ||
-            memberFullInfo.get("fcmToken") === currentMembers.b.token
-          )
-            continue;
-          if (
-            memberFullInfo.get("udid") === currentMembers.c.id ||
-            memberFullInfo.get("fcmToken") === currentMembers.c.token
-          )
-            continue;
-          if (
-            memberFullInfo.get("udid") === currentMembers.d.id ||
-            memberFullInfo.get("fcmToken") === currentMembers.d.token
-          )
-            continue;
+        if (tempCnt == 0) {
+          for (let i = 0; i < n; i++) {
+            const memberFullInfo = snapshot.docs[i];
+            if (memberFullInfo.get("fcmToken") === masterFcmToken) continue;
+            if (
+              memberFullInfo.get("udid") === currentMembers.a.id ||
+              memberFullInfo.get("fcmToken") === currentMembers.a.token
+            )
+              continue;
+            if (
+              memberFullInfo.get("udid") === currentMembers.b.id ||
+              memberFullInfo.get("fcmToken") === currentMembers.b.token
+            )
+              continue;
+            if (
+              memberFullInfo.get("udid") === currentMembers.c.id ||
+              memberFullInfo.get("fcmToken") === currentMembers.c.token
+            )
+              continue;
+            if (
+              memberFullInfo.get("udid") === currentMembers.d.id ||
+              memberFullInfo.get("fcmToken") === currentMembers.d.token
+            )
+              continue;
 
-          switch (tempCnt) {
-            case 0: {
-              addInfoA.id = memberFullInfo.get("udid");
-              addInfoA.token = memberFullInfo.get("fcmToken");
-              addInfoA.userName = memberFullInfo.get("userName");
-              if (typeof addInfoA.token === typeof undefined) {
-                addInfoA.token = "null";
+            switch (i) {
+              case 0: {
+                addInfoA.id = memberFullInfo.get("udid");
+                addInfoA.token = memberFullInfo.get("fcmToken");
+                addInfoA.userName = memberFullInfo.get("userName");
+                if (typeof addInfoA.token === typeof undefined) {
+                  addInfoA.token = "null";
+                }
+                currentMembers.a = addInfoA;
+                break;
               }
-              currentMembers.a = addInfoA;
-              break;
-            }
-            case 1: {
-              addInfoB.id = memberFullInfo.get("udid");
-              addInfoB.token = memberFullInfo.get("fcmToken");
-              addInfoB.userName = memberFullInfo.get("userName");
-              if (typeof addInfoB.token === typeof undefined) {
-                addInfoB.token = "null";
+              case 1: {
+                addInfoB.id = memberFullInfo.get("udid");
+                addInfoB.token = memberFullInfo.get("fcmToken");
+                addInfoB.userName = memberFullInfo.get("userName");
+                if (typeof addInfoB.token === typeof undefined) {
+                  addInfoB.token = "null";
+                }
+                currentMembers.b = addInfoB;
+                break;
               }
-              currentMembers.b = addInfoB;
-              break;
-            }
 
-            case 2: {
-              addInfoC.id = memberFullInfo.get("udid");
-              addInfoC.token = memberFullInfo.get("fcmToken");
-              addInfoC.userName = memberFullInfo.get("userName");
-              if (typeof addInfoC.token === typeof undefined) {
-                addInfoC.token = "null";
+              case 2: {
+                addInfoC.id = memberFullInfo.get("udid");
+                addInfoC.token = memberFullInfo.get("fcmToken");
+                addInfoC.userName = memberFullInfo.get("userName");
+                if (typeof addInfoC.token === typeof undefined) {
+                  addInfoC.token = "null";
+                }
+                currentMembers.c = addInfoC;
+                break;
               }
-              currentMembers.c = addInfoC;
-              break;
-            }
-            case 3: {
-              addInfoD.id = memberFullInfo.get("udid");
-              addInfoD.token = memberFullInfo.get("fcmToken");
-              addInfoD.userName = memberFullInfo.get("userName");
-              if (typeof addInfoD.token === typeof undefined) {
-                addInfoD.token = "null";
+              case 3: {
+                addInfoD.id = memberFullInfo.get("udid");
+                addInfoD.token = memberFullInfo.get("fcmToken");
+                addInfoD.userName = memberFullInfo.get("userName");
+                if (typeof addInfoD.token === typeof undefined) {
+                  addInfoD.token = "null";
+                }
+                currentMembers.d = addInfoD;
+                break;
               }
-              currentMembers.d = addInfoD;
-              break;
-            }
 
-            default: {
-              console.log("okay");
-              break;
+              default: {
+                console.log("okay");
+                break;
+              }
             }
+            tempCnt += 1;
+            if (tempCnt === cnt) break;
           }
-          tempCnt += 1;
-          if (tempCnt == cnt) break;
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log("Error getting documents", err);
       });
 
-    await db
-      .collection("Rooms")
-      .doc(roomId)
-      .set(
-        {
-          videoLink: youtubeLink,
-          startPlay: false,
-          members: currentMembers
-        },
-        { merge: true }
-      );
+    await db.collection("Rooms").doc(roomId).set(
+      {
+        videoLink: youtubeLink,
+        startPlay: false,
+        members: currentMembers,
+      },
+      { merge: true }
+    );
 
     const registrationTokens = [
       currentMembers.a.token,
       currentMembers.b.token,
       currentMembers.c.token,
-      currentMembers.d.token
+      currentMembers.d.token,
     ];
-    const registeredNames = [
-      currentMembers.a.userName,
-      currentMembers.b.userName,
-      currentMembers.c.userName,
-      currentMembers.d.userName
-    ];
+    // const registeredNames = [
+    //   currentMembers.a.userName,
+    //   currentMembers.b.userName,
+    //   currentMembers.c.userName,
+    //   currentMembers.d.userName
+    // ];
 
-    for (let j = 0; j < cnt; j++) {
-      var notificationBody =
-        registeredNames[j] +
-        " invited you to a " +
-        activityType +
-        " Work out, Ready? ";
+    for (let j = 0; j < tempCnt; j++) {
+      const notificationBody =
+        masterName + " invited you to a " + activityType + " Work out, Ready? ";
       const payload = {
         notification: {
           title: "Hey!",
           body: notificationBody,
           sound: "default",
-          click_action: "connected"
+          click_action: "connected",
         },
         data: {
           senderId: roomId,
-          videoLink: youtubeLink
-        }
+          videoLink: youtubeLink,
+        },
       };
       const options = {
-        content_available: true
+        content_available: true,
       };
       await admin
         .messaging()
